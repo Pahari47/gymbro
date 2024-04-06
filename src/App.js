ec9428258fb464693fb0f36fe2ab545bb9e8bc99
Pahari47 <nachiketapahari00@gmail.com>
2024-04-06 17:51:53 +0530

    adding plans and some other features

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 import Hero from './components/Hero/Hero';
+import Plans from './components/Plans/Plans';
+import Program from './components/Programs/Program';
+import Reasons from './components/Reasons/Reasons';
 function App() {
   return (
     <div className="App">
       <Hero/>
+      <Program/>
+      <Reasons/>
+      <Plans/>
     </div>
   );
 }
