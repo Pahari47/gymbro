278d553e13a6e79c1c8c4b01a18ebda55a56d165
Pahari47 <nachiketapahari00@gmail.com>
2024-04-07 00:58:03 +0530

    set emailjs

---
+++ src/components/Join/Join.jsx
@@ -0,0 +1,46 @@
+import React, { useRef } from 'react'
+import './Join.css'
+import emailjs from '@emailjs/browser'
+
+const Join = () => {
+    const form = useRef()
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm('service_g664v6o', 'template_krfoi9c', form.current, {
+                publicKey: 'q5xpN6T_vPSgvGwdx',
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                },
+            );
+    };
+    return (
+        <div className="Join" id="join-us">
+            <div className="left-j">
+                <hr />
+                <div>
+                    <span className='stroke-text'>READY TO</span>
+                    <span>LEVEL UP</span>
+                </div>
+                <div>
+                    <span>YOUR BODY</span>
+                    <span className='stroke-text'>WITH US?</span>
+                </div>
+            </div>
+            <div className="right-j">
+                <form ref={form} className="email-container" onSubmit={sendEmail}>
+                    <input type="email" name='user_email' placeholder='Please enter your email' />
+                    <button className='btn btn-j'>Join Now</button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Join
